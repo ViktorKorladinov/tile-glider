445d225fb9ff638d711b229790e820f4cb4e58c1
ViktorKorladinov <v.korladinov@gmail.com>
2024-07-25 13:29:22 +0200

    Added dev capability, added wait command.

--- app/Grid.js
+++ app/Grid.js
@@ -7,9 +7,9 @@ import './grid.css'
 const CELL_SIZE = 240;
 
 export default function Grid({ m, n, position }) {
-    const [pos, setPos] = useState({ x: 5, y: 5 });
+    const [pos, setPos] = useState({ x: 0, y: 0 });
 
-    const placeTiles2 = () => {
+    const placeTiles = () => {
         let pathData = `
         M0,0
         H${n * CELL_SIZE} V${m * CELL_SIZE} H0 Z
@@ -23,7 +23,7 @@ export default function Grid({ m, n, position }) {
             gridLines.push(<line key={`vert${i}${i}`} x1={i * CELL_SIZE} x2={i * CELL_SIZE} y1={(m - 1) * CELL_SIZE} y2={m * CELL_SIZE} />)
         }
         for (let i = 1; i < n; i++) {
-            gridLines.push(<line key={`h${i}0`}  y1={i * CELL_SIZE} y2={i * CELL_SIZE} x1="0" x2={`${CELL_SIZE}`} />)
+            gridLines.push(<line key={`h${i}0`} y1={i * CELL_SIZE} y2={i * CELL_SIZE} x1="0" x2={`${CELL_SIZE}`} />)
             gridLines.push(<line key={`h${i}${i}`} y1={i * CELL_SIZE} y2={i * CELL_SIZE} x1={(n - 1) * CELL_SIZE} x2={n * CELL_SIZE} />)
         }
 
@@ -40,28 +40,41 @@ export default function Grid({ m, n, position }) {
     }))
 
     useEffect(() => {
-        console.log(pos,(n - 1)*CELL_SIZE);
-        if (pos.y != (n - 1)*CELL_SIZE && pos.y != 0) {
-            console.log('here');
+        if (Number.isInteger(position)) {
             api.start({
                 from: { x: pos.x, y: pos.y },
-                to: [{ x: (position.x) * CELL_SIZE}, { y: (position.y) * CELL_SIZE}].reverse(),
-                onStart: () => setPos({ x: position.x * CELL_SIZE, y: position.y * CELL_SIZE }),
+                to: { x: pos.x + 50, y: pos.y + 50 },
+                onResolve: () => {
+                    api.start({
+                        from: { x: pos.x + 50, y: pos.y + 50 },
+                        to: { x: pos.x, y: pos.y },
+                        delay: position * 250
+                    })
+                }
             })
         } else {
-            api.start({
-                from: { x: pos.x, y: pos.y },
-                to: [{ x: (position.x) * CELL_SIZE }, { y: (position.y) * CELL_SIZE }],
-                onStart: () => setPos({ x: position.x * CELL_SIZE, y: position.y * CELL_SIZE }),
-            })
+            console.log(pos)
+            if (pos.y != (n - 1) * CELL_SIZE && pos.y != 0) {
+                api.start({
+                    from: { x: pos.x, y: pos.y },
+                    to: [{ x: (position.x) * CELL_SIZE }, { y: (position.y) * CELL_SIZE }].reverse(),
+                })
+            } else {
+                api.start({
+                    from: { x: pos.x, y: pos.y },
+                    to: [{ x: (position.x) * CELL_SIZE }, { y: (position.y) * CELL_SIZE }],
+                })
+            }
+            setPos({ x: position.x * CELL_SIZE, y: position.y * CELL_SIZE })
         }
+
+
 
     }, [position])
     return (
-        <svg height={`95vh`} viewBox={`-5 -5 ${n * CELL_SIZE + 6} ${m * CELL_SIZE + 6}`}
+        <svg height={`100vh`} viewBox={`-5 -5 ${n * CELL_SIZE + 10} ${m * CELL_SIZE + 10}`}
             xmlns="http://www.w3.org/2000/svg">
-            {/* {placeTiles()} */}
-            {placeTiles2()}
+            {placeTiles()}
             <animated.rect x={srpingVals.x} y={srpingVals.y} width="113" height="113" className="mover" rx="15" />
         </svg >
     )
--- app/page.js
+++ app/page.js
@@ -2,23 +2,35 @@
 import { useState } from "react"
 import './grid.css'
 import Grid from "./Grid";
-// import sequenceJSON from '../path.json'
+import testData from '../test.json'
 
-export default function Home() {
+export default function Home(props) {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [seq, setSequence] = useState();
   const [size, setSize] = useState({ n: 0, m: 0 });
+  const [testing, setTesting] = useState(true);
+
+  function loadJSON() {
+    if (size.n != 0) return
+    if (testing) {
+      parseJSON(testData)
+      return <></>
+    }
+    return <input type="file" onChange={handleChange} />
+  }
+
+  const parseJSON = (data) => {
+    setSequence(data["paths"][0]);
+    setSize({ n: data.n, m: data.m })
+  }
 
   function handleChange(event) {
     const file = event.target.files[0]
     if (file['type'] === 'application/json') {
       let reader = new FileReader();
+      reader.onload = (event) => parseJSON(JSON.parse(event.target.result))
       reader.readAsText(file);
-      reader.onload = function () {
-        let data = JSON.parse(reader.result);
-        setSequence(data["paths"][0]);
-        setSize({ n: data.n, m: data.m })
-      };
+
     }
   }
 
@@ -26,15 +38,21 @@ export default function Home() {
     if (seq && seq.length > 0) {
       const newCoords = seq[0]
       setSequence(seq.slice(1))
-      setPosition({ x: newCoords[0], y: newCoords[1] })
+      if (Number.isInteger(newCoords)) {
+        setPosition(newCoords)
+      } else {
+        setPosition({ x: newCoords[0], y: newCoords[1] })
+
+      }
     }
   }
 
   return (
     <main>
-      <input type="file" onChange={handleChange} />
-      <button onClick={consumeMove}>Move</button>
-      <Grid n={size.n} m={size.m} position={position} />
+      {loadJSON()}
+      <section className="gridHolder" onClick={consumeMove}>
+        <Grid n={size.n} m={size.m} position={position} />
+      </section>
     </main>
   );
 }
