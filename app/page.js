8c0f12a7a61239760c438915c8c5a4fdf290959d
Viktor Korladinov <v.korladinov@gmail.com>
2024-12-29 11:52:09 +0100

    client side portal

--- app/page.js
+++ app/page.js
@@ -1,23 +1,23 @@
-// 'use client'
-// import {useEffect, useState} from "react";
+'use client'
+import {useEffect, useState} from "react";
 
 
 export default function Home() {
-    // const [parsedSimulations, setParsedSimulations] = useState([]);
-    //
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         const response = await fetch(`/api/parseSimulations`);
-    //         const jsonData = await response.json();
-    //         setParsedSimulations(jsonData);
-    //     };
-    //     void fetchData();
-    // }, []);
+    const [parsedSimulations, setParsedSimulations] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`/api/parseSimulations`);
+            const jsonData = await response.json();
+            setParsedSimulations(jsonData);
+        };
+        void fetchData();
+    }, []);
 
     return (<div>
             <h1>Available Simulations</h1>
             <ul>
-                {[].map((file, id) => (<li key={`simulation-${id}`}>
+                {parsedSimulations.map((file, id) => (<li key={`simulation-${id}`}>
                         <a href={`/${id}/simulator`}>
                             {`Simulation [${file.topology}] with dose ${file.dose}, ${file.movers} movers and ${file.patients} patients`}
                         </a>
