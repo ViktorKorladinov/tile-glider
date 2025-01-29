afcad84efbc4a7f9a9e85e53625df30169451b3b
Viktor Korladinov <v.korladinov@gmail.com>
2025-01-29 12:34:28 +0100

    Changed the way exact positions are calculated in preparation for resting places

--- components/Grid.jsx
+++ components/Grid.jsx
@@ -1,186 +1,228 @@
 // noinspection JSValidateTypes,JSIncompatibleTypesComparison
 
-'use client'
-import {useState, useEffect, useRef, useCallback} from "react"
-import {animated, useSprings} from '@react-spring/web'
-import Tile from "./Tile"
-import Toolbar from "./Toolbar";
-
+'use client';
+import {useState, useEffect, useRef, useCallback} from 'react';
+import {animated, useSprings} from '@react-spring/web';
+import Tile from './Tile';
+import Toolbar from './Toolbar';
 
 const CELL_SIZE = 240;
+const MOVER_SIZE = 112;
 // m x n
 export default function Grid({m, n, simulationData, fill}) {
-    const medicineInfo = simulationData['medicine']
-    const dispenserInfo = simulationData['dispensers']
-    const positions = simulationData['mover_paths']['paths']
-    const patientColors = simulationData['patient_color_dict']
-    const ganttData = simulationData['gantts']
-    const [counter, setCounter] = useState(1)
-    const [speed, setSpeed] = useState(0)
-    const [matrix, setMatrix] = useState(() => {
-        return Array.from({length: m}, () => Array(n).fill(0));
-    });
-    const [selected, setSelected] = useState(() => {
-        return Array.from({length: m}, () => Array(n).fill(0));
-    });
-    const [medicineName, setMedicine] = useState("");
+  const medicineInfo = simulationData['medicine'];
+  const dispenserInfo = simulationData['dispensers'];
+  const patientColors = simulationData['patient_color_dict'];
+  const ganttData = simulationData['gantts'];
+  import { useMemo } from "react";
 
-    const statePosC = positions.map(arr => ({x: arr[0].x * CELL_SIZE, y: arr[0].y * CELL_SIZE}));
-    const [srpingVals, api] = useSprings(positions.length, idx => ({
-        from: statePosC[idx],
-    }))
+  let simulationDatumElement = simulationData['mover_paths']['paths'];
+  const positions = useMemo(() => {
+    const logicalToExactPos = (allPaths) => {
+      return allPaths.map(moverPath => {
+        return moverPath.map(pos => {
+          let newPos = { ...pos };
+          newPos.logicalX = pos.x;
+          newPos.logicalY = pos.y;
+          switch (pos.mode) {
+            case 'wait_rest':
+              newPos.x = pos.x * CELL_SIZE + CELL_SIZE / 2 - MOVER_SIZE / 2;
+              newPos.y = pos.y * CELL_SIZE + CELL_SIZE / 2 - MOVER_SIZE / 2;
+              break;
+            default:
+              newPos.x = pos.x * CELL_SIZE + CELL_SIZE / 2 - MOVER_SIZE / 2;
+              newPos.y = pos.y * CELL_SIZE + CELL_SIZE / 2 - MOVER_SIZE / 2;
+          }
+          return newPos;
+        });
+      });
+    };
 
-    const moversRefs = useRef(statePosC);
-    const requestRef = useRef();
-    const previousTimeRef = useRef();
-    const animateRef = useRef(0);
-    const progressRef = useRef(1);
-    const medicineRef = useRef('');
+    return logicalToExactPos(simulationDatumElement);
+  }, [simulationDatumElement]);
 
-    // react to animation speed change
-    useEffect(() => {
-        animateRef.current = speed
-    }, [speed]);
 
-    // react to medicine selection
-    useEffect(() => {
-        if (medicineName === medicineRef.current) return
-        let updatedSelected = Array.from({length: m}, () => Array(n).fill(0))
-        if (medicineName.length > 0) {
-           for (const singleMedicineName of medicineName.split(',')) {
-               for (let coordinate of medicineInfo[singleMedicineName]) {
-                   const x = coordinate[0]
-                   const y = coordinate[1]
-                   updatedSelected[x][y] = 1
-               }
-           }
-            medicineRef.current = medicineName
-        } else {
-            medicineRef.current = ''
+  const [counter, setCounter] = useState(1);
+  const [speed, setSpeed] = useState(0);
+  const [matrix, setMatrix] = useState(() => {
+    return Array.from({length: m}, () => Array(n).fill(0));
+  });
+  const [selected, setSelected] = useState(() => {
+    return Array.from({length: m}, () => Array(n).fill(0));
+  });
+  const [medicineName, setMedicine] = useState('');
+
+  const statePosC = positions.map(
+      arr => ({x: arr[0].x, y: arr[0].y}));
+  const [srpingVals, api] = useSprings(positions.length, idx => ({
+    from: statePosC[idx],
+  }));
+
+  const moversRefs = useRef(statePosC);
+  const requestRef = useRef();
+  const previousTimeRef = useRef();
+  const animateRef = useRef(0);
+  const progressRef = useRef(1);
+  const medicineRef = useRef('');
+
+  // react to animation speed change
+  useEffect(() => {
+    animateRef.current = speed;
+  }, [speed]);
+
+  // react to medicine selection
+  useEffect(() => {
+    if (medicineName === medicineRef.current) return;
+    let updatedSelected = Array.from({length: m}, () => Array(n).fill(0));
+    if (medicineName.length > 0) {
+      for (const singleMedicineName of medicineName.split(',')) {
+        for (let coordinate of medicineInfo[singleMedicineName]) {
+          const x = coordinate[0];
+          const y = coordinate[1];
+          updatedSelected[x][y] = 1;
         }
-        setSelected(updatedSelected);
-    }, [medicineInfo, matrix, m, medicineName, n, selected])
-
-    const consumeMove = useCallback(() => {
-        if (positions && positions.length > 0 && progressRef.current !== positions[0].length - 1) {
-            let newCoords = []
-            for (const path of positions) {
-                const nextStep = path[progressRef.current + 1];
-                newCoords.push(nextStep)
-            }
-            api.start(index => {
-                const position = newCoords[index]
-                let res;
-                const pos = moversRefs.current
-                if (pos.y !== (n - 1) * CELL_SIZE && pos.y !== 0) {
-                    res = {
-                        from: {x: pos.x, y: pos.y},
-                        to: [{x: (position.x) * CELL_SIZE}, {y: (position.y) * CELL_SIZE}].reverse(),
-                    }
-                } else {
-                    res = {
-                        from: {x: pos.x, y: pos.y}, to: [{x: (position.x) * CELL_SIZE}, {y: (position.y) * CELL_SIZE}],
-                    }
-                }
-                moversRefs.current[index] = {x: position.x * CELL_SIZE, y: position.y * CELL_SIZE}
-                return {...res, config: {duration: animateRef.current}}
-            })
-            progressRef.current += 1
-            setCounter(ct => ct + 1)
-            console.log(matrix)
-            const updatedMatrix = [...matrix]; // Update heatmap
-            for (const pos of newCoords) {
-                const {x, y, mode} = pos
-                if (mode !== "transit"){
-                    continue
-                }
-                updatedMatrix[x][y] += 1 ;
-            }
-            setMatrix(updatedMatrix);
-        }
-    }, [api, matrix, n, positions])
-
-    const animateV = useCallback(time => {
-        if (previousTimeRef.current !== undefined) {
-            const deltaTime = time - previousTimeRef.current;
-            if (animateRef.current > 0 && deltaTime > animateRef.current) {
-                consumeMove()
-                previousTimeRef.current = time;
-            }
-        } else {
-            previousTimeRef.current = time;
-        }
-        if (progressRef.current !== positions[0].length) requestRef.current = requestAnimationFrame(animateV); else progressRef.current -= 1
-    }, [consumeMove, positions])
-
-    useEffect(() => {
-        window.abc = (val) => {
-            const name = val['points'][0]['data']['offsetgroup']
-            if (name in medicineInfo) {
-                setMedicine(name)
-            }
-        }
-        window.reset = () => {
-            setMedicine("")
-        }
-        window.legend = (data) => {
-            setMedicine(medicine => {
-                const newMed = data['data'][data['curveNumber']]['legendgroup']
-                if (medicine === newMed) {
-                    return ""
-                } else {
-                    return newMed
-                }
-            });
-        }
-        requestRef.current = requestAnimationFrame(animateV);
-        return () => cancelAnimationFrame(requestRef.current);
-    }, [animateV, medicineInfo]);
-
-
-    const placeTiles = () => {
-        let res = []
-        for (let i = 0; i < m; i++) {
-            for (let j = 0; j < n; j++) {
-                if (fill || i === 0 || j === 0 || i === m - 1 || j === n - 1) {
-                    res.push(<Tile maxPath={ganttData['max_path']} setMedicine={setMedicine} selected={selected[i][j]} speed={speed} key={`${i}x${j}`}
-                                   name={dispenserInfo[`${i}x${j}`]}
-                                   w={CELL_SIZE} x={i * CELL_SIZE} y={j * CELL_SIZE} idx={matrix[i][j]}/>)
-                }
-            }
-        }
-        return res
+      }
+      medicineRef.current = medicineName;
+    } else {
+      medicineRef.current = '';
     }
+    setSelected(updatedSelected);
+  }, [medicineInfo, matrix, m, medicineName, n, selected]);
 
-    return (<>
-        <svg viewBox={`-5 -5 ${m * CELL_SIZE + 10} ${n * CELL_SIZE + 10}`}
-             xmlns="http://www.w3.org/2000/svg">
-            <defs>
-                {srpingVals.map((spring, id) => (
-                    <pattern key={`def${id}`} id={`bgPattern${id}`} patternUnits="userSpaceOnUse" width="20"
-                             height="20">
+  const consumeMove = useCallback(() => {
+    if (positions && positions.length > 0 && progressRef.current !==
+        positions[0].length - 1) {
+      let newCoords = [];
+      for (const path of positions) {
+        const nextStep = path[progressRef.current + 1];
+        newCoords.push(nextStep);
+      }
+      api.start(index => {
+        const position = newCoords[index];
+        let res;
+        const pos = moversRefs.current;
+        if (pos.y !== (n - 1) * CELL_SIZE && pos.y !== 0) {
+          res = {
+            from: {x: pos.x, y: pos.y},
+            to: [
+              {x: (position.x)},
+              {y: (position.y)}].reverse(),
+          };
+        } else {
+          res = {
+            from: {x: pos.x, y: pos.y},
+            to: [{x: (position.x)}, {y: (position.y)}],
+          };
+        }
+        moversRefs.current[index] = {
+          x: position.x,
+          y: position.y,
+        };
+        return {...res, config: {duration: animateRef.current}};
+      });
+      progressRef.current += 1;
+      setCounter(ct => ct + 1);
+      const updatedMatrix = [...matrix]; // Update heatmap
+      for (const pos of newCoords) {
+        const {logicalX, logicalY, mode} = pos;
+        if (mode !== 'transit') {
+          continue;
+        }
+        updatedMatrix[logicalX][logicalY] += 1;
+      }
+      setMatrix(updatedMatrix);
+    }
+  }, [api, matrix, n, positions]);
 
-                        <rect width="20" height="20"
-                              fill={patientColors[positions[id][progressRef.current]['patient']]}/>
-                        <path d='M-1,1 l2,-2
+  const animateV = useCallback(time => {
+    if (previousTimeRef.current !== undefined) {
+      const deltaTime = time - previousTimeRef.current;
+      if (animateRef.current > 0 && deltaTime > animateRef.current) {
+        consumeMove();
+        previousTimeRef.current = time;
+      }
+    } else {
+      previousTimeRef.current = time;
+    }
+    if (progressRef.current !==
+        positions[0].length) requestRef.current = requestAnimationFrame(
+        animateV); else progressRef.current -= 1;
+  }, [consumeMove, positions]);
+
+  useEffect(() => {
+    window.abc = (val) => {
+      const name = val['points'][0]['data']['offsetgroup'];
+      if (name in medicineInfo) {
+        setMedicine(name);
+      }
+    };
+    window.reset = () => {
+      setMedicine('');
+    };
+    window.legend = (data) => {
+      setMedicine(medicine => {
+        const newMed = data['data'][data['curveNumber']]['legendgroup'];
+        if (medicine === newMed) {
+          return '';
+        } else {
+          return newMed;
+        }
+      });
+    };
+    requestRef.current = requestAnimationFrame(animateV);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, [animateV, medicineInfo]);
+
+  const placeTiles = () => {
+    let res = [];
+    for (let i = 0; i < m; i++) {
+      for (let j = 0; j < n; j++) {
+        if (fill || i === 0 || j === 0 || i === m - 1 || j === n - 1) {
+          res.push(<Tile maxPath={ganttData['max_path']}
+                         setMedicine={setMedicine} selected={selected[i][j]}
+                         speed={speed} key={`${i}x${j}`}
+                         name={dispenserInfo[`${i}x${j}`]}
+                         w={CELL_SIZE} x={i * CELL_SIZE} y={j * CELL_SIZE}
+                         idx={matrix[i][j]}/>);
+        }
+      }
+    }
+    return res;
+  };
+
+  return (<>
+    <svg viewBox={`-5 -5 ${m * CELL_SIZE + 10} ${n * CELL_SIZE + 10}`}
+         xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        {srpingVals.map((spring, id) => (
+            <pattern key={`def${id}`} id={`bgPattern${id}`}
+                     patternUnits="userSpaceOnUse" width="20"
+                     height="20">
+
+              <rect width="20" height="20"
+                    fill={patientColors[positions[id][progressRef.current]['patient']]}/>
+              <path d="M-1,1 l2,-2
                         M0,20 l20,-20
-                        M19,21 l2,-2'
-                              stroke='black' strokeWidth='4'/>
-                    </pattern>))}
-            </defs>
+                        M19,21 l2,-2"
+                    stroke="black" strokeWidth="4"/>
+            </pattern>))}
+      </defs>
 
-            <g id="gridSvg">
-                {placeTiles()}
-            </g>
-            {srpingVals.map((spring, id) => {
-                return (
-                    <animated.rect key={`mover${id}`} x={spring['x']} y={spring['y']} width="112" height="112" style={{
-                        fill: `url(#bgPattern${id})`
-                    }} rx="15"/>)
-            })}
-        </svg>
-        <Toolbar counter={counter} length={positions[0].length} animate={speed} medicineName={medicineName}
-                 consumeMove={consumeMove} setAnimate={setSpeed} setMedicine={setMedicine} ganttData={ganttData} />
-    </>)
+      <g id="gridSvg">
+        {placeTiles()}
+      </g>
+      {srpingVals.map((spring, id) => {
+        return (
+            <animated.rect key={`mover${id}`} x={spring['x']} y={spring['y']}
+                           width={MOVER_SIZE} height={MOVER_SIZE} style={{
+              fill: `url(#bgPattern${id})`,
+            }} rx="15"/>);
+      })}
+    </svg>
+    <Toolbar counter={counter} length={positions[0].length} animate={speed}
+             medicineName={medicineName}
+             consumeMove={consumeMove} setAnimate={setSpeed}
+             setMedicine={setMedicine} ganttData={ganttData}/>
+  </>);
 
 }
--- components/Tile.jsx
+++ components/Tile.jsx
@@ -1,49 +1,51 @@
-import React, {memo, useEffect, useRef, useState} from 'react'
+import React, {memo, useEffect, useRef, useState} from 'react';
 import {getColorFromGradient} from '@/utils/colorUtilities';
 
 const Tile = memo(function NonMemoTile({
-                                           w,
-                                           h = w,
-                                           x,
-                                           y,
-                                           idx,
-                                           selected,
-                                           maxPath,
-                                           setMedicine,
-                                           name = 'Interface',
-                                           speed
-                                       }) {
-    let color = getColorFromGradient(idx, maxPath)
-    let tileType = "tile"
-    if (selected === 1) {
-        color = 'lightgreen'
-        tileType += 'selected'
-    } else if (name === 'Interface') {
-        tileType += " interface"
-        color = 'lightblue'
-    }
-    const [stateY, setStateY] = useState(y)
-    const [stateX, setStateX] = useState(x)
-    const trans = `fill ${speed}ms linear`
-    const textRef = useRef(null);
+  w,
+  h = w,
+  x,
+  y,
+  idx,
+  selected,
+  maxPath,
+  setMedicine,
+  name = 'Interface',
+  speed,
+}) {
+  let color = getColorFromGradient(idx, maxPath);
+  let tileType = 'tile';
+  if (selected === 1) {
+    color = 'lightgreen';
+    tileType += 'selected';
+  } else if (name === 'Interface') {
+    tileType += ' interface';
+    color = 'lightblue';
+  }
+  const [stateY, setStateY] = useState(y);
+  const [stateX, setStateX] = useState(x);
+  const trans = `fill ${speed}ms linear`;
+  const textRef = useRef(null);
 
-    useEffect(() => {
-        const textElement = textRef.current;
-        const textBBox = textElement.getBBox();
-        const textWidth = textBBox.width;
-        setStateX(x + w / 2 - textWidth / 2)
-        setStateY(y + h / 2 + 40)
-    }, [h, y, idx, x, w]);
-    return (<g className={tileType} onMouseEnter={() => {
-        setMedicine(name)
-    }} onMouseLeave={() => {
-        setMedicine("")
-    }}>
-        <rect y={y} x={x} width={w} height={h} fill={color}
-              style={{transition: trans, msTransition: trans, WebkitTransition: trans}}/>
-        <text ref={textRef} y={stateY} x={stateX} width={w} height={h} fontFamily="Verdana" fontSize="120"
-              fill='black'>{idx}</text>
-    </g>)
-})
+  useEffect(() => {
+    setStateX(x + 10);
+    setStateY(y + 60);
+  }, [x, y]);
+  return (<g className={tileType} onMouseEnter={() => {
+    setMedicine(name);
+  }} onMouseLeave={() => {
+    setMedicine('');
+  }}>
+    <rect y={y} x={x} width={w} height={h} fill={color}
+          style={{
+            transition: trans,
+            msTransition: trans,
+            WebkitTransition: trans,
+          }}/>
+    <text ref={textRef} y={stateY} x={stateX} width={w} height={h}
+          fontFamily="Verdana" fontSize="50"
+          fill="black">{idx}</text>
+  </g>);
+});
 
-export default Tile
+export default Tile;
